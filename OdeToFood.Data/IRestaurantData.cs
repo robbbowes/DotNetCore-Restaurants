30baffa6d61b7da5173f7609b3d5683b016aa6f1
robbbowes <up666724@myport.ac.uk>
2020-04-06 18:01:39 +0200

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pizza Addy", Location="Sarreguemines", Cuisine=CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "City Döner", Location="Mannheim", Cuisine=CuisineType.Turkish },
+                new Restaurant { Id = 3, Name = "La Tasca", Location="Kingston", Cuisine=CuisineType.Mexican }
+            };
+
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+                   
+        }
+    }
+}
